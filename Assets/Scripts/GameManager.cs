1b1a276734286b4f48e270e8cd32e7511230f20e
Sebastien Feser <sedukafe@gmail.com>
2019-10-13 10:24:16 +0200

    lobby scripts created and added Photon Pun

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+using Photon.Pun;
+using Photon.Realtime;
+
+public class GameManager : MonoBehaviourPunCallbacks
+{
+    #region Photon Callbacks
+
+    public override void OnLeftRoom()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public override void OnPlayerEnteredRoom(Player newPlayer)
+    {
+        Debug.LogFormat("Player ", newPlayer.NickName, " entered the room");
+        if (PhotonNetwork.IsMasterClient)
+        {
+            Debug.LogFormat("IsMasterClient ", PhotonNetwork.IsMasterClient);
+
+            LoadGameScene();
+        }
+    }
+
+    public override void OnPlayerLeftRoom(Player otherPlayer)
+    {
+        Debug.LogFormat("Player ", otherPlayer.NickName, " left the room");
+        if (PhotonNetwork.IsMasterClient)
+        {
+            Debug.LogFormat("IsMasterClient ", PhotonNetwork.IsMasterClient);
+
+            LoadGameScene();
+        }
+    }
+
+    #endregion
+
+    #region Public Methods
+
+    public void LeaveRoom()
+    {
+        PhotonNetwork.LeaveRoom();
+    }
+
+    #endregion
+
+    #region Private Methods
+
+    void LoadGameScene()
+    {
+        if (!PhotonNetwork.IsMasterClient)
+        {
+            Debug.LogError("Trying to load level but we are not the master Client");
+        }
+
+        PhotonNetwork.LoadLevel("GameScene");
+
+    }
+    #endregion
+}
---
+++ Assets/Scripts/Launcher.cs
@@ -0,0 +1,96 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+
+public class Launcher : MonoBehaviourPunCallbacks
+{
+
+    #region Private Serializable Fields
+    [SerializeField] private byte maxPlayerPerRoom = 4;
+    [SerializeField] GameObject identificationUI;
+    [SerializeField] GameObject connectingText;
+    #endregion
+
+    #region Pivate Fields
+    bool isConnecting;
+
+    //Client's version number
+    string gameVersion = "1";
+    #endregion
+
+    #region MonoBehaviour CallBacks
+
+    private void Awake()
+    {
+        //For clients to sync their level automatically
+        PhotonNetwork.AutomaticallySyncScene = true;
+    }
+
+    private void Start()
+    {
+        identificationUI.SetActive(true);
+        connectingText.SetActive(false);
+    }
+
+    #endregion
+
+    #region Public Methods
+
+    public void Connect()
+    {
+        isConnecting = true;
+        identificationUI.SetActive(false);
+        connectingText.SetActive(true);
+        if (PhotonNetwork.IsConnected)
+        {
+            //To change if you want to create lobbies
+            PhotonNetwork.JoinRandomRoom();
+        }
+        else
+        {
+            PhotonNetwork.GameVersion = gameVersion;
+            PhotonNetwork.ConnectUsingSettings();
+        }
+    }
+
+    #endregion
+
+    #region Pun Callbacks
+
+    public override void OnConnectedToMaster()
+    {
+        Debug.Log("OnConnectedToMaster was called");
+
+        if (isConnecting)
+        {
+            //To change if you'd like to create lobbys
+            PhotonNetwork.JoinRandomRoom();
+        }
+    }
+
+    public override void OnDisconnected(DisconnectCause cause)
+    {
+        connectingText.SetActive(false);
+        identificationUI.SetActive(true);
+        Debug.LogWarningFormat("OnDisconnected() was called by PUN with reason { 0} ", cause);
+    }
+
+    public override void OnJoinRandomFailed(short returnCode, string message)
+    {
+        Debug.Log("Join Random Room Failed, Creation of a new Room");
+        PhotonNetwork.CreateRoom(null, new RoomOptions() { MaxPlayers = maxPlayerPerRoom });
+        
+    }
+
+    public override void OnJoinedRoom()
+    {
+        Debug.Log("Client joined a room");
+
+        PhotonNetwork.LoadLevel("GameRoom");
+    }
+
+    #endregion
+
+}
